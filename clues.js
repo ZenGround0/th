8e402fae0af7f2d57838283581ccaff07660b22d
zenground0 <ZenGround0@users.noreply.github.com>
2025-07-11 00:31:17 +0200

    cleaup clue map

---
+++ clues.js
@@ -0,0 +1,12 @@
+const CLUES_MAP = {
+    "kinkajou": "clue2.html",
+    "turkey-anteater": "clue3.html",
+    "hand-basin": "clue4.html",
+    "sit-on-us": "clue5.html",
+    "yucca-sego-lily": "clue6.html",
+    "zhur": "clue7.html",
+    "cavansite": "clue8.html",
+    "ribosome": "final.html"
+};
+
+window.CLUES_MAP = CLUES_MAP;
